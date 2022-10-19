b6ccfeeb53683e65c36593019c2728ba6811d32d
yaoqijun <yaoqijunmail@foxmail.com>
2022-10-19 15:13:50 +0800

    add all

---
+++ template/go.mod
@@ -0,0 +1,5 @@
+module github.com/LiveAlone/GoLibSourceAnalyse/template
+
+go 1.19
+
+require github.com/emirpasic/gods v1.18.1
---
+++ template/gods_test.go
@@ -0,0 +1,22 @@
+package template
+
+import (
+	"fmt"
+	"github.com/emirpasic/gods/lists/arraylist"
+	"github.com/emirpasic/gods/sets/hashset"
+	"testing"
+)
+
+func TestNone(t *testing.T) {
+	list := arraylist.New()
+	list.Add("a")      // ["a"]
+	list.Add("c", "b") // ["a","c","b"]
+	fmt.Println(list)
+}
+
+func TestSet(t *testing.T) {
+	set := hashset.New()
+	set.Add(123, 12, 345)
+	set.Add(12, 34, 56)
+	fmt.Println(set)
+}
