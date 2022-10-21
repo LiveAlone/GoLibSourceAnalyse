9e8b8f562077e90a3bfec21e46d8269895bedfba
yaoqijun <yaoqijunmail@foxmail.com>
2022-10-21 18:54:04 +0800

    add all

--- leetcode/main_test.go
+++ leetcode/main_test.go
@@ -5,5 +5,4 @@ import (
 )
 
 func TestBasic(t *testing.T) {
-
 }
--- lib/open/eddycjy/basic_test.go
+++ lib/open/eddycjy/basic_test.go
@@ -16,3 +16,20 @@ func TestStringEmpty(t *testing.T) {
 func TestCut(t *testing.T) {
 	fmt.Println(strings.Cut("yaoqijunmail@foxmail", "@"))
 }
+
+// nil 可以调用
+
+type T struct{}
+
+func (t *T) Hello() string {
+	//if t == nil {
+	//	fmt.Println("脑子进煎鱼了")
+	//	return ""
+	//}
+	return "煎鱼进脑子了"
+}
+
+func TestPrintNil(t *testing.T) {
+	var et T
+	fmt.Println(et.Hello())
+}
