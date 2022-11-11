7b35143d9739887e6865cc785a9090a3b8b087b1
yaoqijun <yaoqijunmail@foxmail.com>
2022-11-11 16:38:33 +0800

    add all

---
+++ http2/main/h1Server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	srv := &http.Server{Addr: ":8000", Handler: http.HandlerFunc(handle)}
+
+	log.Printf("Serving on https://0.0.0.0:8000")
+	log.Fatal(srv.ListenAndServeTLS("data/server.crt", "data/server.key"))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Got connection: %s", r.Proto)
+	ct, err := w.Write([]byte("Hello World"))
+	log.Printf("ct is %v, error %v", ct, err)
+}
