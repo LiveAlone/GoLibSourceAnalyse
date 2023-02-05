73bbe649905a57b6be1253cfdb3098b6cd1af156
yaoqijun <yaoqijunmail@foxmail.com>
2023-02-06 00:10:06 +0800

    append

--- leetcode/main_test.go
+++ leetcode/main_test.go
@@ -6,3 +6,26 @@ import (
 
 func TestBasic(t *testing.T) {
 }
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+
+	max := 1
+	pre := 1
+	for index := 1; index < len(s); index++ {
+		currentSize := 1
+		for i := 1; i <= pre; i++ {
+			if s[index-i] == s[index] {
+				break
+			}
+			currentSize += 1
+		}
+		pre = currentSize
+		if currentSize > max {
+			max = currentSize
+		}
+	}
+	return max
+}
