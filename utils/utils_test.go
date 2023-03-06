d96cc921d01e338620cc5bf590b7bdfbda905a68
yaoqijun <yaoqijunmail@foxmail.com>
2023-03-06 15:50:52 +0800

    add all

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,18 +1,8 @@
 package main
 
 import (
-	"log"
 	"testing"
 )
 
-type Person struct {
-	Name string
-}
-
 func TestNone(t *testing.T) {
-	//p := &Person{
-	//	Name: "123",
-	//}
-	var p *Person
-	log.Fatalf("p is :%v", *p)
 }
