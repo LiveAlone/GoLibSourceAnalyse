7ece47f10643719c3330317e1e0e8a60cb575888
yaoqijun <yaoqijunmail@foxmail.com>
2023-03-16 18:32:51 +0800

    add

--- utils/manager/api/model.go
+++ utils/manager/api/model.go
@@ -1,2 +1,22 @@
 // Package api api 接口模型
 package api
+
+type HttpApi struct {
+	Schema           string
+	Path             string
+	Method           string
+	Prefix           string // 接口标识
+	Description      string
+	RequestBodyType  string
+	ResponseBodyType string
+}
+
+// BodyDesc 结构体描述信息
+type BodyDesc struct {
+	Name       string // 字段名称
+	Type       string // 基础类型, 结构体类型, todo swagger format
+	Example    string
+	Required   bool
+	Array      bool
+	Properties []*BodyDesc
+}
