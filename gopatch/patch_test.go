beaee98a0a9b944ee14251391ef6eda93553a4fd
yaoqijun <yaoqijunmail@foxmail.com>
2023-03-20 19:06:03 +0800

    all

--- gopatch/patch_test.go
+++ gopatch/patch_test.go
@@ -8,5 +8,21 @@ import (
 
 func TestNone(t *testing.T) {
 	err := errors.New(fmt.Sprintf("hello"))
+	// err := fmt.Errorf("hello")
 	fmt.Println(err)
+}
+
+func TestT2(t *testing.T) {
+	foo(3)
+	var x = 100
+	foo(x)
+	foo(1 + 2 + 3 + 4 + 5)
+}
+
+func foo(v int) int {
+	return v
+}
+
+func bar(v int, exist bool) int {
+	return v
 }
