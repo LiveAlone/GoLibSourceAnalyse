2efc7fe92e9ea9976f41c10127879d2325c2e826
yaoqijun <yaoqijunmail@foxmail.com>
2023-04-17 21:29:54 +0800

    pre fix

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"testing"
-	"time"
 )
 
 func TestNone(t *testing.T) {
-	text := "20230901"
-	nt, err := time.Parse("20060102", text)
-	fmt.Println(nt.Unix(), err)
 }
