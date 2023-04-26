aa2c52541b63b81fb52c99fa624bd199a350a81a
yaoqijun <yaoqijunmail@foxmail.com>
2023-04-26 18:58:23 +0800

    add

--- ubergo/dig/dig_test.go
+++ ubergo/dig/dig_test.go
@@ -2,11 +2,15 @@ package dig
 
 import (
 	"encoding/json"
+	"fmt"
 	"go.uber.org/dig"
 	"log"
 	"os"
 	"testing"
 )
+
+type TestOpt struct {
+}
 
 func TestFirst(t *testing.T) {
 	type Config struct {
@@ -16,10 +20,8 @@ func TestFirst(t *testing.T) {
 	c := dig.New()
 	var err error
 
-	// Provide a Config object. This can fail to decode.
 	err = c.Provide(func() (*Config, error) {
-		// In a real program, the configuration will probably be read from a
-		// file.
+		fmt.Println(".......stating Config")
 		var cfg Config
 		err := json.Unmarshal([]byte(`{"prefix": "[foo] "}`), &cfg)
 		return &cfg, err
@@ -28,19 +30,23 @@ func TestFirst(t *testing.T) {
 		log.Fatalf("Failed to provide config: %v", err)
 	}
 
-	// Provide a way to build the logger based on the configuration.
 	err = c.Provide(func(cfg *Config) *log.Logger {
+		fmt.Println(".......stating logger")
 		return log.New(os.Stdout, cfg.Prefix, 0)
 	})
 	if err != nil {
 		log.Fatalf("Failed to provide logger: %v", err)
 	}
 
-	// Invoke a function that requires the logger, which in turn builds the
-	// Config first.
-	err = c.Invoke(func(l *log.Logger) {
-		l.Print("You've been invoked")
-	})
+	fmt.Println(".......stating invoke")
+	for i := 0; i < 300; i++ {
+		go func() {
+			err = c.Invoke(func(l *log.Logger) {
+				l.Print("You've been invoked")
+			})
+		}()
+	}
+
 	if err != nil {
 		log.Fatalf("Failed to invoke: %v", err)
 	}
