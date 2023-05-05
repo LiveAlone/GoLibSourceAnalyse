c2dc219c85571d63a863807cae8db1e111c21ae2
yaoqijun <yaoqijunmail@foxmail.com>
2023-05-05 10:57:17 +0800

    fix all

--- gomock/office/calcuate_test.go
+++ gomock/office/calcuate_test.go
@@ -1,6 +1,7 @@
 package office
 
 import (
+	"github.com/LiveAlone/GoLibSourceAnalyse/gomock/mock_office"
 	"github.com/golang/mock/gomock"
 	"testing"
 )
@@ -10,20 +11,20 @@ func TestCal(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	//// 创建mock对象
-	//mockCalculator := NewMockCalculator(ctrl)
-	//
-	//// 预期Calculator的Add方法将被调用一次，参数为2和3，返回值为5
-	//mockCalculator.EXPECT().Add(2, 3).Return(5)
-	//
-	//// 预期Calculator的Subtract方法将被调用一次，参数为2和3，返回值为-1
-	//mockCalculator.EXPECT().Subtract(2, 3).Return(-1)
-	//
-	//// 调用DoubleCalculator，传入mockCalculator作为参数
-	//result := DoubleCalculator(mockCalculator, 2, 3)
-	//
-	//// 检查DoubleCalculator的返回值是否为预期的4
-	//if result != 4 {
-	//	t.Errorf("Expected 4, but got %d", result)
-	//}
+	// 创建mock对象
+	mockCalculator := mock_office.NewMockCalculator(ctrl)
+
+	// 预期Calculator的Add方法将被调用一次，参数为2和3，返回值为5
+	mockCalculator.EXPECT().Add(2, 3).Return(5)
+
+	// 预期Calculator的Subtract方法将被调用一次，参数为2和3，返回值为-1
+	mockCalculator.EXPECT().Subtract(2, 3).Return(-1)
+
+	// 调用DoubleCalculator，传入mockCalculator作为参数
+	result := DoubleCalculator(mockCalculator, 2, 3)
+
+	// 检查DoubleCalculator的返回值是否为预期的4
+	if result != 5 {
+		t.Errorf("Expected 4, but got %d", result)
+	}
 }
